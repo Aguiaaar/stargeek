975fea2fb7bbab08dc6d33a7cbf1617cf1e7fedb
Ana Laura Aguiar Gonçales <ana.goncales@portalsesisp.org.br>
2023-05-30 11:26:27 -0300

    30052023

--- login.js
+++ login.js
@@ -4,28 +4,31 @@ const email = document.getElementById ("imail");
 const senha = document.getElementById ("isenha");
 
 formulario.onsubmit = (evento) =>{
+    let logado;
     let dados = JSON.parse(localStorage.getItem("dados"));
    
     dados.forEach((elemento) => {
-        if (elemento.email === email.value && elemento.senha === senha.value){
-            evento.preventDefault();
-            let dados = JSON.parse(sessionStorage.getItem("logado")) || [];
-            dados.push(
-                {
-                    email :email.value
-                }
-            )
-            sessionStorage.setItem("logado", JSON.stringify(dados));
-            window.location.assign("minhalista.html");   
-            alert("Perfil logado com sucesso");
-            return true;
+        console.log(elemento.email+" "+email.value+" "+ elemento.senha+" "+ senha.value);
+        if (elemento.email == email.value && elemento.senha == senha.value){
+            logado = true;
+           
         }
-        else{
-            evento.preventDefault();
-            senha.value = "";
-            alert("E-mail ou Senha estão inválidos");
-        }
-
     });
+    if (logado){
+        evento.preventDefault();
+        let dados = JSON.parse(sessionStorage.getItem("logado")) || [];
+        dados.push(
+            {
+                email :email.value
+            }
+        )
+        sessionStorage.setItem("logado", JSON.stringify(dados));
+        window.location.assign("minhalista.html");   
+        alert("Perfil logado com sucesso");
+    }
+    else{
+        evento.preventDefault();
+        alert("E-mail ou Senha estão inválidos");
+    }
 }
 
